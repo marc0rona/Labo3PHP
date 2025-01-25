27ea15c6dd3450246ea722423a0b6b4d86f9e63a
marcorona <marc0mourr96@gmail.com>
2025-01-25 18:55:18 -0500

    Add forms + HTML template pour les 4 Exercices

--- Exercice1/index.php
+++ Exercice1/index.php
@@ -1,3 +1,26 @@
 <?php
-  echo "main page";
+  //Fetch date d'auj. pour le mettre automatiquement dans le input quand la page se Load
+  $currDate = date('Y-m-d');
 ?>
+
+<!-- FRONT-END : HTML -->
+<!DOCTYPE html>
+<html>
+<head> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title> Exercice 1 </title>
+</head>
+<body>
+<h2> BIBLIOTHÈQUE </h2>
+  <form method="POST">
+    <label for="titre">Entrez le titre du livre :</label>
+    <input type="text" id="titre" name="titre" required>
+    <br><br> <!--Input : date d'emprunt-->
+    <label for="InitDate">Date d'emprunt : </label>
+    <!-- value = auj. // minimum = 'auj' (ne peut pas choisir une date du passé) -->
+    <input type="date" id="InitDate" name="InitDate" value="<?php echo $currDate;?>" min="<?php echo $currDate;?>"> 
+    <br><br>
+    <button type="submit"> Confirmation d'Emprunt </button>
+  </form>
+</body>
+</html> 
--- Exercice4/index.php
+++ Exercice4/index.php
@@ -1,3 +1,30 @@
 <?php
-  echo "Page index";
+   //Fetch date d'auj. pour le mettre automatiquement dans le input quand la page se Load
+   $currDate = date('Y-m-d');
 ?>
+
+<!-- FRONT-END : HTML -->
+<!DOCTYPE html>
+<html>
+<head> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title> Exercice 4 </title>
+</head>
+<body>
+<h2> Entrez une tâche : </h2>
+  <form method="POST">
+    <label for="titre">Entrez le nom de la tache :</label>
+    <input type="text" id="titre" name="titre" required>
+    <br><br> <!--Input : date d'emprunt-->
+    <label for="InitDate">Date Initiale : </label>
+    <!-- value = auj. // minimum = 'auj' (ne peut pas choisir une date du passé) -->
+    <input type="date" id="InitDate" name="InitDate" value="<?php echo $currDate;?>" min="<?php echo $currDate;?>"> 
+    <br><br>
+    <label for="EndDate" style="color: red;">Deadline : </label>
+    <!-- ToDo : que le deadline != dateInitiale -->
+    <input type="date" id="EndDate" name="EndDate" min="<?php echo $currDate;?>"> 
+    <br><br>
+    <button type="submit"> Ajout de tâche </button>
+  </form>
+</body>
+</html> 
